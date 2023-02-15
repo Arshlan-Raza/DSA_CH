2f2b249c1ebd00f9ffe5c223f63aa04308c8c919
Arshlan <arshalanraja@gmail.com>
2023-02-16 00:38:34 +0530

    Example of 2d array

---
+++ 2darry.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int arr[3][4];
+
+    cout<<"Enter the 2d array elements : ";
+
+    //col wise input 
+    // for(int col = 0; col<4; col++){
+    //     for(int row = 0;  row<3; row++){
+
+    //          cin>>arr[row][col];
+    //     }
+
+    //     cout<<endl;
+    // }
+
+    //row wise input 
+     for(int row = 0; row<3; row++){
+        for(int col = 0;  col<4; col++){
+
+             cin>>arr[row][col];
+        }
+
+        cout<<endl;
+    }
+
+    //printing the array elements 
+    cout<<"The array elements are : "<<endl;
+
+      for(int row = 0; row<3; row++){
+        for(int col = 0; col<4; col++){
+
+           cout<<arr[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
