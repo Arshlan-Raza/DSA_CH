0eedaeebc944e419b92e2f7075bcc8130bf235b6
Arshlan <arshalanraja@gmail.com>
2023-02-16 00:43:36 +0530

    row element sum

---
+++ rowwisesum.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void rowSum(int arr[][4], int n , int m ){
+
+    for(int i = 0 ; i<3; i++){
+        
+        int sum = 0;
+
+        for(int j = 0; j<4; j++){
+
+            sum+= arr[i][j];
+        }
+        cout<<"The sum of "<<i+1<<" is "<<sum<<endl;
+    }
+}
+
+int main(){
+
+    int arr[3][4];
+
+    cout<<"Enter the 2d array elements : ";
+ 
+     for(int row = 0; row<3; row++){
+        for(int col = 0;  col<4; col++){
+
+             cin>>arr[row][col];
+        }
+
+    }
+
+    cout<<"The array elements are : "<<endl;
+
+      for(int row = 0; row<3; row++){
+        for(int col = 0;  col<4; col++){
+
+             cout<<arr[row][col]<<" ";
+        }
+        cout<<endl;
+
+    }
+     cout<<endl;
+
+    cout<<"The row wise sum is : "<<endl;
+
+    cout<<endl;
+
+    rowSum(arr,3,4);
+
+}
