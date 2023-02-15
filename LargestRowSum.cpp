5924842d5d89a50a4a6f8c01893da39d56333de6
Arshlan <arshalanraja@gmail.com>
2023-02-16 00:40:25 +0530

    it is to find the largest of sum of elements of row

---
+++ LargestRowSum.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void rowSum(int arr[][4], int n , int m ){
+
+    for(int i = 0 ; i<3; i++){
+        
+        int sum = 0;
+
+        for(int j = 0; j<4; j++){
+
+            sum+= arr[i][j];
+        }
+        cout<<"The sum of "<<i+1<<" is "<<sum<<endl;
+    }
+}
+
+int lagestRowSum(int arr[][4], int n , int m ){
+
+    int maxi = INT_MIN;
+    int rowIndex = -1;
+
+    for(int i = 0 ; i<3; i++){
+        
+        int sum = 0;
+
+        for(int j = 0; j<4; j++){
+
+            sum+= arr[i][j];
+        }
+        if(sum > maxi){
+            maxi = sum;
+            rowIndex = i;
+        }
+    }
+    return rowIndex;
+}
+
+int main(){
+
+    int arr[3][4];
+
+    cout<<"Enter the 2d array elements : ";
+ 
+     for(int row = 0; row<3; row++){
+        for(int col = 0;  col<4; col++){
+
+             cin>>arr[row][col];
+        }
+
+    }
+
+    cout<<"The array elements are : "<<endl;
+
+      for(int row = 0; row<3; row++){
+        for(int col = 0;  col<4; col++){
+
+             cout<<arr[row][col]<<" ";
+        }
+        cout<<endl;
+
+    }
+     cout<<endl;
+
+    cout<<"The row wise sum is : "<<endl;
+
+    cout<<endl;
+
+    rowSum(arr,3,4);
+
+    cout<<"The index of largest sum of row is "<<lagestRowSum(arr,3,4);
+}
