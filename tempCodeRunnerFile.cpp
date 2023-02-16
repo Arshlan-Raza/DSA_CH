904f9a477c9c46aeee2d62aea04917e65dc6a966
Arshlan <arshalanraja@gmail.com>
2023-02-16 22:10:13 +0530

    i dont know about this code

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,9 @@
-for(int i=0; i<ns.size(); i++){
-    //     cout<<ns[i]<<endl;
+cout<<"The array elements are : "<<endl;
+
+    //   for(int row = 0; row<3; row++){
+    //     for(int col = 0; col<4; col++){
+
+    //        cout<<arr[row][col]<<" ";
+    //     }
+    //     cout<<endl;
     // }
