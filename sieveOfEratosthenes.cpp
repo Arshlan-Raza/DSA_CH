e22a50760d0094c1ae68003f9bcfd3b47e1a51dd
Arshlan <arshalanraja@gmail.com>
2023-02-17 21:46:11 +0530

    to find all prime number till a given number using this theorem

---
+++ sieveOfEratosthenes.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+    
+    public:
+        int prime(int n){
+
+            int cnt = 0;
+            vector <bool> prime(n+1, true);
+
+            prime[0] = prime[1] = false;
+
+            for(int i = 2; i<n; i++){
+
+                if(prime[i]){
+                    cnt++;
+                }
+
+                for(int j = 2*i; j<n; j=j+i){
+
+                    prime[j] = false;
+
+                }
+            }
+            return cnt;
+        }
+};
+
+int main(){
+
+    int m;
+
+    cout<<"Enter the number : ";
+    cin>>m;
+
+    Solution s;
+    cout<<"Number of prime number is : "<<s.prime(m);
+}
