337f30a5c78b3f98b164671ee7ee1729768b7140
Arshlan <arshalanraja@gmail.com>
2023-02-21 22:37:30 +0530

    playing with macros

---
+++ macros.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define AREA(l,b) (l*b)
+
+int main(){
+
+    int l1,b1;
+    cin>>l1>>b1;
+
+    int area = AREA(l1,b1);
+
+    cout<<"Area is : "<<area;
+}
