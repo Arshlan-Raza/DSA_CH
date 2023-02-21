a483a419be1097ee0ddab511380462219aba1ab3
Arshlan <arshalanraja@gmail.com>
2023-02-21 22:37:43 +0530

    playing with macros 2

---
+++ macros2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define min(a,b)(((a)<(b))?(a):(b))
+
+int main(){
+
+    int x, y;
+    cin>>x>>y;
+
+    cout<<"The minimum value is : "<<min(x,y);
+}
