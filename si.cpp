6d514385ab75ee21ab5cf546cd0292427915e94f
Arshlan <arshalanraja@gmail.com>
2023-02-22 19:33:15 +0530

    this is a very simple code studio question

---
+++ si.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    
+    int p ,t;
+    float r;
+
+    cin>>p>>r>>t;
+
+    int si;
+    
+    si = p*r*t;
+
+    cout<<si;
+
+    return 0;
+}
